9f06d7539f53cf22177bda3b1edf6d9882e30cc2
Anindya Manna <anindyamanna92@gmail.com>
2016-05-31 03:27:27 +0530

    Update task4b.py

--- task4b.py
+++ task4b.py
@@ -2,10 +2,17 @@ class task4b:
     listOfCoordinates = list()
         
     def calcArea():
+        count = 0
         if len(task4b.listOfCoordinates)<1:
             task4b.enterFileName()
         for i in task4b.listOfCoordinates:
-            coordinates = list(map(int,i.split()))
+            if count==0:
+                count+=1
+                continue
+            line = i.split(',')
+            uid = line[0]
+            uname = line[1]
+            coordinates = [float(i) for i in line[2:]]
             x = coordinates[::2]
             y = coordinates[1::2]
             area = 0
@@ -13,7 +20,7 @@ class task4b:
             for j in range(n):
                 area += (x[j]*y[j+1]) - (x[j+1]*y[j])
             area+=(x[n]*y[0]) - (x[0]*y[n])
-            print("The area of the polygon is "+str(area/2))
+            print(uid+" "+uname+" "+str(area/2))
         task4b.listOfCoordinates.close()
 
     def enterFileName():
@@ -27,8 +34,6 @@ class task4b:
         print("Enter file name where file contains\nlist of space seperated coordinate values\neg.. x1 y1 x2 y2 ... xn yn")
         
 
-
-        
 if __name__ == "__main__":
     task4b.task4b_usage()
     task4b.calcArea()
